4a9f83a540ea7ae30f478c82936ab3b5a554ca97
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-06-19 15:27:12 +0800

    Add Vercel Banner

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,38 @@
 import "../styles/globals.css";
 import { ThemeProvider } from "theme-ui";
 import theme from "../lib/theme";
-import { Container, Flex, Text } from "theme-ui";
+import { Container, Box, Text } from "theme-ui";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
       <Container sx={{ maxWidth: "800px", py: "24px", px: "24px" }}>
         <Component {...pageProps} />
-        <Flex columns={2}>
-        <Text as="i" sx={{ fontSize: "12px" }}>
+        <Box
+          columns={2}
+          sx={{ alignContent: "center", display: ["block", "flex"] }}
+        >
+          <Text
+            as="p"
+            sx={{
+              fontSize: "12px",
+              verticalAlign: "text-bottom",
+              minWidth: ["100%", "1%"],
+            }}
+          >
             Innovation Circuit is fiscally sponsored by The Hack Foundation.
             Nonprofit EIN: 81-2908499.
           </Text>
-          <Text sx={{ fontSize: "12px" }}>Powered by ▲ Vercel</Text>
-        </Flex>
+          <Text
+            sx={{ fontSize: "12px", flexGrow: 1, textAlign: ["left", "right"], mt: [2, 0], transform: ['', 'translateY(-2px)'] }}
+            as="p"
+          >
+            <span style={{ background: "#43326F", padding: "2px 6px" }}>
+              Powered by ▲ Vercel
+            </span>
+          </Text>
+        </Box>
       </Container>
-      
     </ThemeProvider>
   );
 }
